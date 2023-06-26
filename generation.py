67500ac3eca124d47a1a3f1a58844cb3b4c7ec55
bryanchrist <brc4cb@virginia.edu>
2023-06-26 09:48:14 -0400

    remove gradient clipping

--- generation.py
+++ generation.py
@@ -134,22 +134,22 @@ tf.compat.v1.disable_eager_execution()
 # however, to compile the model, we still define it
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
 
-# Define placeholder for batch size
-batch_size = tf.shape(tokens)[0]
+# # Define placeholder for batch size
+# batch_size = tf.shape(tokens)[0]
 
-# Placeholder for labels
-labels = tf.compat.v1.placeholder(shape=(batch_size, seq_length), dtype=tf.int32)  # Replace `batch_size` with the actual batch size
+# # Placeholder for labels
+# labels = tf.compat.v1.placeholder(shape=(batch_size, seq_length), dtype=tf.int32)  # Replace `batch_size` with the actual batch size
 
-# Calculate loss
-loss_value = loss(labels, logits)
+# # Calculate loss
+# loss_value = loss(labels, logits)
 
-with tf.GradientTape() as tape:
-    logits = model(tokens)
-    loss_value = loss(labels, logits)
+# with tf.GradientTape() as tape:
+#     logits = model(tokens)
+#     loss_value = loss(labels, logits)
 
-grads = tape.gradient(loss_value, model.trainable_variables)
-clipped_grads, _ = tf.clip_by_global_norm(grads, 0.25)
-train_op = optimizer.apply_gradients(zip(clipped_grads, model.trainable_variables))
+# grads = tape.gradient(loss_value, model.trainable_variables)
+# clipped_grads, _ = tf.clip_by_global_norm(grads, 0.25)
+# train_op = optimizer.apply_gradients(zip(clipped_grads, model.trainable_variables))
 
 
 
